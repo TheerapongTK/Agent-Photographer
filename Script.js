e565af854e835b1a281f5398a0579a468c837635
TheerapongTK <mokmok.447@gmail.com>
2024-04-04 01:47:37 +0700

    Update Sign

--- Script.js
+++ Script.js
@@ -183,17 +183,3 @@ function showSlider(type){
         next.click();
     }, timeAutoNext)
 }
-
-// Sign In/Up
-
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
-
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
-
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
